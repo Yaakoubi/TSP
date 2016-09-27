b63e382d7d124aa872c6eafe42328b652c2de559
Almeun <alexandremeunier.a@gmail.com>
2016-09-26 21:57:19 -0400

    Phase 1 : Fin modif test_graph (j'ai juste enlevé les commentaires ^^)

--- test_graph.py
+++ test_graph.py
@@ -26,7 +26,7 @@ class TestgraphMethods(unittest.TestCase):
                 e = Edge(name='E from {0:d} to {1:d}'.format(id_start, id_arrival), data=e_data)
                 self._gra_test.add_edge(e)
                 self._gra_test.add_to_dict(e)
-                self._edg_test2 = e # self._edg_test2 prendra comme valeur le dernier arrete
+                self._edg_test2 = e # self._edg_test2 prendra comme valeur la derniere arete
 
 
 
@@ -40,7 +40,7 @@ class TestgraphMethods(unittest.TestCase):
     def test_add_and_get_node(self):
         """ Verifie si les noeuds entrees s'ajoutent en fin de liste et s'ils correspondent a un type Node """
         self.failUnless(self._gra_test.get_node(-1) )== self.nod_test2
-        #self.assertRaises(AssertionError,self._gra_test.add_node, "Ceci n'est pas un noeud")
+        self.assertRaises(AssertionError,self._gra_test.add_node, "Ceci n'est pas un noeud")
 
     def test_get_nodes(self):
         """Verifie si la liste de noeuds renvoie bien des objets de type noeuds et si ceux-ci sont bien ordonnes """
@@ -57,22 +57,16 @@ class TestgraphMethods(unittest.TestCase):
         self.assertEqual(self._gra_test.get_nb_edges(), len(self._gra_test.get_edges()))
         self.assertEqual(self._gra_test.get_nb_nodes(), len(self._gra_test.get_nodes()))
 
-
-
-    # A FINIR !!!!
     def test_get_edge_id_from_dict(self):
         """ Verifie le bon fonctionnement de la fonction get_edge_id_from_dict """
         self._gra_test.get_edge_id_from_dict(1, 4)
         self._gra_test.get_edge_id_from_dict(1, 1)
-        #self._gra_test.get_edge_id_from_dict(1, -1)
         self._gra_test.get_edge_id_from_dict(4, 1)
 
-    # A FINIR !!!!!
     def test_get_edge_from_dict(self):
         """ Verifie le bon fonctionnement de la fonction get_edge_from_dict """
         self._gra_test.get_edge_from_dict(1, 4)
         self._gra_test.get_edge_from_dict(1, 1)
-        #self._gra_test.get_edge_from_dict(1, -1)
         self._gra_test.get_edge_from_dict(4, 1)
 
 
